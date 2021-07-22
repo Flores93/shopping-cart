50e13be69e8974abd94936cb225981bc7127ae92
Jose Luis Flores Fuentes <floresfuentes93@gmail.com>
2021-07-22 16:44:44 -0500

    minor changes

--- components/CarritoIcon.tsx
+++ components/CarritoIcon.tsx
@@ -1,8 +1,11 @@
-const Carrito = () => {
+type CarritoIconProps = {
+  size?: string
+}
+const Carrito = ({ size = '4' }: CarritoIconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="inline-block h-4 w-4"
+      className={`inline-block h-${size} w-${size}`}
       viewBox="0 0 20 20"
       fill="currentColor"
     >
