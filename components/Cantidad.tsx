1885f60adcba7eed081f5c8e77c0e3ce7daba69c
Jose Luis Flores Fuentes <floresfuentes93@gmail.com>
2021-07-24 17:20:28 -0500

    minus and plus components created

--- components/Cantidad.tsx
+++ components/Cantidad.tsx
@@ -1,5 +1,7 @@
 import { updateCantidadProducto } from '../context/actions/shoppingCartActions'
 import useShoppingCartContext from '../hooks/useShoppingCartContext'
+import MinusIcon from './MinusIcon'
+import PlusIcon from './PlusIcon'
 
 type CantidadProps = { cantidad: number; productoId: number }
 
@@ -19,20 +21,7 @@ const Cantidad = ({ productoId, cantidad }: CantidadProps) => {
           onClick={() => handleCantidad(cantidad - 1)}
           className="text-blue-700"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
+          <MinusIcon />
         </button>
       )}
       <span className="m-4">{cantidad}</span>
@@ -41,20 +30,7 @@ const Cantidad = ({ productoId, cantidad }: CantidadProps) => {
         onClick={() => handleCantidad(cantidad + 1)}
         className="text-blue-700"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
+        <PlusIcon />
       </button>
     </>
   )
